6cdae1fbbb02e1bc5857ddd421db5f396c2945ea
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-07-23 15:21:01 -0400

    Finished part of Task 1

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ class Airplane {
 // 👇 COMPLETE YOUR WORK BELOW 👇
 */
 
+
+
 /*
   TASK 1
     - Write a Person class whose constructor initializes `name` and `age` from arguments.
@@ -41,8 +43,27 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
 
+  eat(someFood){
+    if( this.stomach.length < 10 ){
+      this.stomach.push(someFood);
+    }
+  }
+  poop(){
+
+  }
+  toString(){
+
+  }
 }
+
+
+
 
 /*
   TASK 2
@@ -62,6 +83,9 @@ class Car {
 
 }
 
+
+
+
 /*
   TASK 3
     - Write a Lambdasian class.
@@ -74,9 +98,13 @@ class Car {
         + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
         + {name} and {location} of course come from the instance's own properties.
 */
+
 class Lambdasian {
 
 }
+
+
+
 
 /*
   TASK 4
@@ -92,9 +120,13 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
+
 class Instructor {
 
 }
+
+
+
 
 /*
   TASK 5
@@ -111,9 +143,13 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
+
 class Student {
 
 }
+
+
+
 
 /*
   TASK 6
@@ -128,9 +164,13 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
+
 class ProjectManager {
 
 }
+
+
+
 
 /*
   STRETCH PROBLEM (no tests!)
@@ -140,6 +180,8 @@ class ProjectManager {
       + This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
       + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
 */
+
+
 
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
